649830391174db2261eaa6fcafa712a554e7b993
dannyserena <dannyserena@gmail.com>
2021-01-02 19:13:03 -0300

    adicionando home e header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,27 @@
+
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
-  selector: 'app-home',
+  selector: 'gt-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  animations: [
+    trigger('homeAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translateY(-20px)'}),
+        animate('300ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
-
+  homeState = 'ready';
+ //imagePath = '/assets/img/welcome.png';
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
